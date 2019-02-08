09a3d4631358ce3e4c8cb1e83d5f531e01a4d605
root <root@knopa.in.ua>
2019-02-08 09:52:03 -0500

    deploy

---
+++ precache-manifest.d76ecf7168587a192ee6a2746b4bee22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-project/robots.txt"
+  },
+  {
+    "revision": "cc8554dbfffcb86cb61a",
+    "url": "/my-project/js/chunk-vendors.43f99170.js"
+  },
+  {
+    "revision": "ce3a6a0d9b7cca52773e",
+    "url": "/my-project/js/app.fd8deede.js"
+  },
+  {
+    "revision": "ba8156be8b1b5bd4f19a",
+    "url": "/my-project/js/about.0d02e657.js"
+  },
+  {
+    "revision": "cfd9fe0ca5bf6323490f5c28847ba91d",
+    "url": "/my-project/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/my-project/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "cc8554dbfffcb86cb61a",
+    "url": "/my-project/css/chunk-vendors.2da46af1.css"
+  },
+  {
+    "revision": "ce3a6a0d9b7cca52773e",
+    "url": "/my-project/css/app.fdd75ee0.css"
+  }
+];
